8c12cfb9880a5a6979aa0d31b1efedb4675399fe
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-04-05 20:44:51 -0300

    início css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Navbar.css'
+import logoBar from './bar-do-alemao-logo.gif'
 
 class Navbar extends React.Component {
 	constructor(props) {
@@ -11,14 +13,18 @@ class Navbar extends React.Component {
 
 		return (
 			<header>
-				<nav className="">
-					<Link to="/">início</Link>
-					<Link to="/historia">história</Link>
-					<Link to="/programacao">programação</Link>
-					<Link to="/galeria">galeria</Link>
-					<Link to="/cardapio">cardápio</Link>
-					<Link to="/informacoes">informações</Link>
+				<nav className={styles.navbar}>
+					<ul className={styles.menu}>
+						<Link className={styles.menuLink} to="/">início</Link>
+						<Link className={styles.menuLink} to="/historia">história</Link>
+						<Link className={styles.menuLink} to="/programacao">programação</Link>
+						<Link className={styles.menuLink} to="/galeria">galeria</Link>
+						<Link className={styles.menuLink} to="/cardapio">cardápio</Link>
+						<Link className={styles.menuLink} to="/informacoes">informações</Link>
+					</ul>
 				</nav>
+				<img className={styles.logoBar} src={logoBar} alt="Logo do Bar do Alemão"/>
+				<span className={styles.age} >50 anos</span>
 			</header>
 		)
 	}
