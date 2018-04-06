cb22b38d652b007d75deddab6bd79e20fb5bedd5
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-04-06 17:44:42 -0300

    Navbar posicionamento OK

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,29 +2,43 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navbar.css'
 import logoBar from './bar-do-alemao-logo.gif'
+import FaBars from 'react-icons/lib/fa/bars'
+import FaClose from 'react-icons/lib/fa/close'
 
 class Navbar extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = { menuOpen: false }
+		this.toggleMenu = this.toggleMenu.bind(this)
+	}
 
+	toggleMenu() {
+		event.stopPropagation()
+		this.setState({ menuOpen: !this.state.menuOpen })
 	}
 
 	render() {
-
+		console.log(this.state.menuOpen)
 		return (
 			<header>
 				<nav className={styles.navbar}>
-					<ul className={styles.menu}>
-						<Link className={styles.menuLink} to="/">início</Link>
-						<Link className={styles.menuLink} to="/historia">história</Link>
-						<Link className={styles.menuLink} to="/programacao">programação</Link>
-						<Link className={styles.menuLink} to="/galeria">galeria</Link>
-						<Link className={styles.menuLink} to="/cardapio">cardápio</Link>
-						<Link className={styles.menuLink} to="/informacoes">informações</Link>
+					<div className={styles.barInfo} >
+						<span className={styles.duration} >1968 - 2018</span>
+						<img className={styles.logoBar} src={logoBar} alt="Logo do Bar do Alemão" />
+						<span className={styles.age} >50 anos</span>
+					</div>
+					<span className={styles.switch} onClick={this.toggleMenu}>
+						{this.state.menuOpen ? <FaClose className={styles.close} /> : <FaBars className={styles.hamburguer} />}
+					</span>
+					<ul className={this.state.menuOpen ? styles.menuOpen : styles.menu}>
+						<Link className={styles.menuLink} to="/" onClick={this.toggleMenu}>início</Link>
+						<Link className={styles.menuLink} to="/historia" onClick={this.toggleMenu}>história</Link>
+						<Link className={styles.menuLink} to="/programacao" onClick={this.toggleMenu}>programação</Link>
+						<Link className={styles.menuLink} to="/galeria" onClick={this.toggleMenu}>galeria</Link>
+						<Link className={styles.menuLink} to="/cardapio" onClick={this.toggleMenu}>cardápio</Link>
+						<Link className={styles.menuLink} to="/informacoes" onClick={this.toggleMenu}>informações</Link>
 					</ul>
 				</nav>
-				<img className={styles.logoBar} src={logoBar} alt="Logo do Bar do Alemão"/>
-				<span className={styles.age} >50 anos</span>
 			</header>
 		)
 	}
