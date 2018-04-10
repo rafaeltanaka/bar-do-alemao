4550afda2bc4cfecaa487f127ae024f2d11c9c3c
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-04-09 22:15:02 -0300

    Working carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -5,29 +5,55 @@ import image3 from './5ff6bb_f1a513ef283941528aeac6c7b3afd540_mv2.jpg'
 import styles from './Carousel.css'
 
 class Carousel extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props)
-		this.state = { selectedImg: 'image1' }
+		this.state = { selectedImg: '', interval: 0 }
+	}
+
+	componentDidMount() {
+		this.changeImage('image1')
+		this.interval = setInterval(() => this.nextImage(), 5000)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.interval)
+	}
+
+	nextImage() {
+		switch (this.state.selectedImg) {
+		case 'image2':
+			this.changeImage('image3')
+			break
+		case 'image3':
+			this.changeImage('image1')
+			break
+		case 'image1':
+			this.changeImage('image2')
+			break
+		default:
+			break
+		}
 	}
 
 	changeImage(imageIndex) {
 		this.setState({ selectedImg: imageIndex })
-		console.log(this.state.selectedImg)
 	}
 
 	render() {
 
 		return (
-			<div className={styles.carousel} >
-				<img className={this.state.selectedImg == 'image1' ? styles.imageSelected : styles.imageHidden} src={image1} />
-				<img className={this.state.selectedImg == 'image2' ? styles.imageSelected : styles.imageHidden} src={image2} />
-				<img className={this.state.selectedImg == 'image3' ? styles.imageSelected : styles.imageHidden} src={image3} />
-				<div>
-					<input type="radio" name="selectImage" onClick={() => this.changeImage('image1')} />
-					<input type="radio" name="selectImage" onClick={() => this.changeImage('image2')} />
-					<input type="radio" name="selectImage" onClick={() => this.changeImage('image3')} />
+			<React.Fragment>
+				<div className={styles.carousel} >
+					<img className={this.state.selectedImg == 'image1' ? styles.imageSelected : styles.imageHidden} src={image1} />
+					<img className={this.state.selectedImg == 'image2' ? styles.imageSelected : styles.imageHidden} src={image2} />
+					<img className={this.state.selectedImg == 'image3' ? styles.imageSelected : styles.imageHidden} src={image3} />
 				</div>
-			</div>
+				<div className={styles.selectors}>
+					<label className={this.state.selectedImg == 'image1' ? styles.imgSelectorOn : styles.imgSelectorOff} onClick={() => this.changeImage('image1')} checked={this.state.selectedImg == 'image1'}> </label>
+					<label className={this.state.selectedImg == 'image2' ? styles.imgSelectorOn : styles.imgSelectorOff} onClick={() => this.changeImage('image2')} checked={this.state.selectedImg == 'image2'}> </label>
+					<label className={this.state.selectedImg == 'image3' ? styles.imgSelectorOn : styles.imgSelectorOff} onClick={() => this.changeImage('image3')} checked={this.state.selectedImg == 'image3'}> </label>
+				</div>
+			</React.Fragment >
 		)
 	}
 }
