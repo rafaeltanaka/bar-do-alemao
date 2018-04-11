7366caaf5169c2138f5c09b9beb2f58e922739f9
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-04-11 02:24:54 -0300

    Informacoes OK

--- src/components/Pages/Informacoes/Informacoes.js
+++ src/components/Pages/Informacoes/Informacoes.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Infos from '../../Infos/Infos'
+import styles from './Informacoes.css'
 
 const Informacoes = () => (
-	<h1>Informações</h1>
+	<section className={styles.info}>
+		<h1 className={styles.title}>Informações</h1>
+		<Infos />
+	</section>
 )
 
 export default Informacoes
