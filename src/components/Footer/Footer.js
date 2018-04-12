9dc93325128b48ba5d43c75c803be1b3148a4eb0
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-04-11 22:05:59 -0300

    Added quick menu on footer (mobile)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styles from './Footer.css'
-import FaFacebook from 'react-icons/lib/fa/facebook'
-import FaYoutubePlay from 'react-icons/lib/fa/youtube-play'
-import FaInstagram from 'react-icons/lib/fa/instagram'
+import facebook from './facebook.png'
+import instagram from './instagram.png'
+import youtube from './youtube.png'
+import FaPhone from 'react-icons/lib/fa/phone'
+import FaEnvelope from 'react-icons/lib/fa/envelope'
+import FaFacebookOfficial from 'react-icons/lib/fa/facebook-official'
 
 class Footer extends React.Component {
 	constructor(props) {
@@ -40,14 +43,14 @@ class Footer extends React.Component {
 							</div>
 						</div>
 						<div className={styles.social} >
-							<a className={styles.link} href="https://www.facebook.com/obardoalemao/" target="blank" >
-								<FaFacebook className={styles.socialIcons} />
+							<a className={styles.link} href="https://www.facebook.com/obardoalemao/" target="blank" rel="noopener noreferrer" >
+								<img className={styles.socialIcons} src={facebook} />
 							</a>
-							<a className={styles.link} href="https://www.youtube.com/channel/UCSkmPWslGhBPneMu0S_IQ6w" target="blank" >
-								<FaYoutubePlay className={styles.socialIcons} />
+							<a className={styles.link} href="https://www.youtube.com/channel/UCSkmPWslGhBPneMu0S_IQ6w" target="blank" rel="noopener noreferrer" >
+								<img className={styles.socialIcons} src={youtube} />
 							</a>
-							<a className={styles.link} href="https://www.instagram.com/obardoalemao/" target="blank" >
-								<FaInstagram className={styles.socialIcons} />
+							<a className={styles.link} href="https://www.instagram.com/obardoalemao/" target="blank" rel="noopener noreferrer" >
+								<img className={styles.socialIcons} src={instagram} />
 							</a>
 						</div>
 					</div>
@@ -57,6 +60,17 @@ class Footer extends React.Component {
 						<input type="email" placeholder="email" className={styles.inputEmail} />
 						<input type="button" value="OK" className={styles.formButton} />
 					</form>
+				</div>
+				<div className={styles.mobileFooter} >
+					<a href="tel:+55113672-1859" className={styles.contactLinks} >
+						<FaPhone className={styles.footerIcons} />
+					</a>
+					<a href="mailto:contato.bardoalemao@gmail.com" className={styles.contactLinks} >
+						<FaEnvelope className={styles.footerIcons} />
+					</a>
+					<a href="https://www.facebook.com/obardoalemao" className={styles.contactLinks} target="blank" rel="noopener noreferrer" >
+						<FaFacebookOfficial className={styles.footerIcons} />
+					</a>
 				</div>
 			</footer>
 		)
